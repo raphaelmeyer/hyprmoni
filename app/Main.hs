e1fb2ac6dd961f60c352826ce5828508d0690341
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-08-13 18:47:56 +0200

    split application and ui

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import qualified App
+import qualified Application
 import qualified Control.Exception as Exception
 import qualified Hyprland
 import qualified System.Exit as System
@@ -10,7 +10,7 @@ main = do
   Exception.handle onError $ do
     monitors <- Hyprland.allMonitors
     print monitors
-    App.run monitors
+    Application.run monitors
 
 onError :: Exception.SomeException -> IO a
 onError e = do
---
+++ app/UI.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module UI (draw, attributes, Name, State (..), Selection (..)) where
+
+import qualified Brick as Util
+import qualified Brick.AttrMap as Attr
+import qualified Brick.Types as T
+import qualified Brick.Widgets.Border as Border
+import qualified Brick.Widgets.Center as Center
+import qualified Brick.Widgets.Core as Core
+import qualified Data.Text as Text
+import qualified Graphics.Vty as Vty
+import qualified Types
+
+appTitle :: Text.Text
+appTitle = "hyprmoni"
+
+data Selection = Selection
+  { selectedMonitor :: Text.Text,
+    selectedMode :: Text.Text
+  }
+
+data State = State
+  { sMonitors :: [Types.MonitorInfo],
+    sSelected :: Selection
+  }
+
+data Name = Name Text.Text deriving (Eq, Ord, Show)
+
+draw :: State -> [T.Widget Name]
+draw state =
+  [ Core.hBox
+      [ Border.borderWithLabel
+          (Core.txt appTitle)
+          ( Core.padLeft (Core.Pad 1) . Core.padRight (Core.Pad 2) . Core.padTopBottom 1 . Core.hBox $
+              map (drawMonitor $ sSelected state) (sMonitors state)
+          )
+      ]
+  ]
+
+drawMonitor :: Selection -> Types.MonitorInfo -> T.Widget Name
+drawMonitor selection monitor =
+  Core.padLeft (Core.Pad 1)
+    . Border.borderWithLabel (drawMonitorBorder selection monitor)
+    . Core.vLimit 5
+    . Core.hLimit 16
+    . Center.center
+    $ drawModes (selectedMode selection) monitor
+
+drawMonitorBorder :: Selection -> Types.MonitorInfo -> T.Widget Name
+drawMonitorBorder selection monitor =
+  if selectedMonitor selection == Types.name monitor
+    then Core.withAttr aSelected title
+    else title
+  where
+    title = Core.txt . Types.name $ monitor
+
+drawModes :: Text.Text -> Types.MonitorInfo -> T.Widget Name
+drawModes selected monitor =
+  let name = Types.name monitor
+      current = Types.mode monitor
+      available = Types.available monitor
+   in Core.vBox
+        [ Core.vLimit 3 . Core.viewport (Name name) T.Vertical . Core.vBox $
+            map (drawMode current selected) available
+        ]
+
+drawMode :: Text.Text -> Text.Text -> Text.Text -> T.Widget n
+drawMode current selected mode =
+  Core.padRight (Core.Pad 1)
+    . Core.padLeft Core.Max
+    . Core.withAttr style
+    . visible
+    $ Core.txt mode
+  where
+    style
+      | mode == selected && mode == current = aSelected <> aCurrent
+      | mode == selected = aSelected
+      | mode == current = aCurrent
+      | otherwise = aDefault
+    visible = if mode == selected then Core.visible else id
+
+aSelected :: Attr.AttrName
+aSelected = Attr.attrName "selected"
+
+aCurrent :: Attr.AttrName
+aCurrent = Attr.attrName "current"
+
+aDefault :: Attr.AttrName
+aDefault = Attr.attrName "default"
+
+attributes :: Attr.AttrMap
+attributes =
+  Attr.attrMap
+    Vty.defAttr
+    [ (aSelected, Util.fg Vty.cyan),
+      (aCurrent, Util.fg Vty.yellow),
+      (aSelected <> aCurrent, Util.fg Vty.magenta)
+    ]
